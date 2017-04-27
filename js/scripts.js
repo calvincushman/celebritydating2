371228c180d7cbbddcdef37c3887ac44f84faba5
Calvin Cushman and Karina Roush <calvin@>
2017-04-27 16:26:22 -0700

    fix bugs, spruce up css

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,38 @@
 $(function(){
-  $(".btn").click(function(){
+  $(".btn").click(function(){ debugger;
     $(".celebrity").hide();
 
     var hair = $("#hair").val()
     var zodiac = $("#zodiac").val()
 
-    if(hair === blonde || black && zodiac === gemini || sagittarius) {
-      $(".tina").show()
-    }
-    else if(hair <= 24 && zodiac === virgo || aries || leo) {
-      $(".doc").show()
-    }
-    else if(hair <= 24 && zodiac === virgo || aries || leo) {
-      $(".doc").show()
-    }
-    else if(hair <= 24 && zodiac === virgo || aries || leo) {
-      $(".doc").show()
-    }
-    else if(hair <= 24 && zodiac === virgo || aries || leo) {
-      $(".doc").show()
+    if(hair === "Blonde" || hair === "Black") {
+      if(zodiac === "Aquarius" || zodiac === "Pisces" || zodiac === "Aries" || zodiac === "Taurus" || zodiac === "Gemini" || zodiac === "Cancer") {
+        $(".tina").show()
+      }
     }
 
+    if(hair === "Blonde" || hair === "Black") {
+      if(zodiac === "Leo" || zodiac === "Virgo" || zodiac === "Libra" || zodiac === "Scorpio" || zodiac === "Sagittarius" || zodiac === "Capricorn") {
+        $(".denise").show()
+      }
+    }
 
+    if(hair === "Brown" || hair === "Red") {
+      if(zodiac === "Aquarius" || zodiac === "Pisces" || zodiac === "Aries" || zodiac === "Taurus" || zodiac === "Gemini" || zodiac === "Cancer") {
+        $(".chris").show()
+      }
+    }
 
+    if(hair === "Brown" || hair === "Red") {
+      if(zodiac === "Leo" || zodiac === "Virgo" || zodiac === "Libra" || zodiac === "Scorpio" || zodiac === "Sagittarius" || zodiac === "Capricorn") {
+        $(".xena").show()
+      }
+    }
+
+    if (hair === "Blue") {
+      $(".doc").show()
+    }
 
   });
-});
+  });
+// });
