65a94110cd7c3c99209712dc18e116d76cb43a19
Calvin Cushman and Karina Roush <karina@>
2017-04-27 14:42:13 -0700

    change zodiac and hair to dropdown menus, begin writing js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 $(function(){
   $(".btn").click(function(){
-     $(".celebrity").hide();
+    $(".celebrity").hide();
+
+    var hair = $("#hair").val()
+    var zodiac = $("#zodiac").val()
+
+    if(hair === blonde || black && zodiac === gemini || sagittarius) {
+      $(".tina").show()
+    }
+    else if(hair <= 24 && zodiac === virgo || aries || leo) {
+      $(".doc").show()
+    }
+    else if(hair <= 24 && zodiac === virgo || aries || leo) {
+      $(".doc").show()
+    }
+    else if(hair <= 24 && zodiac === virgo || aries || leo) {
+      $(".doc").show()
+    }
+    else if(hair <= 24 && zodiac === virgo || aries || leo) {
+      $(".doc").show()
+    }
 
 
+
+
+  });
 });
